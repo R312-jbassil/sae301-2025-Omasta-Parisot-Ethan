85f5d14de14479c633b7ec50caf9f9dc022396ca
Omasta-Parisot-Ethan <ethan.omasta-parisot@edu.univ-fcomte.fr>
2025-11-05 16:49:25 +0100

    commit node et config installé

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: node({
+    mode: 'standalone', // permet d'exécuter le serveur directement avec Node/PM2
+  }),
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
